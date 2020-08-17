4c538bf27e5cb81f0a0ca26077908398f942ca40
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-08-18 01:21:03 +1000

    implemented movement inversion and sprite now follows movement direction

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -15,6 +15,9 @@ public class CharacterController : MonoBehaviour
     public float jumpBuffer = 1f;
 
     [SerializeField]
+    public bool invertDirection = false;
+
+    [SerializeField]
     private LayerMask platformLayerMask;
 
     private Rigidbody2D rb;
@@ -30,8 +33,15 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
         //Horizontal movement
-        movement = Input.GetAxis("Horizontal");
+        movement = Input.GetAxis("Horizontal") * (invertDirection ?  -1 : 1);
+        if (movement < 0f){
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y);
+        }
+        else if (movement > 0f){
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
         rb.velocity = new Vector2(movement * moveSpeed, rb.velocity.y);
+
 
         //Jumping
         if (isGrounded() && Input.GetButtonDown("Jump")){
