9d967549ee61be282856cba7240f631419add623
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-08-20 22:05:06 +1000

    added prototype jumpPad

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -6,6 +6,7 @@ public class JumpPad : MonoBehaviour
 {
     public float jumpSpeed = 20f;
 
+
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player"){
