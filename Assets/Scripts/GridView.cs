7dbd8d32e7ca95eb6ecbf3c0ac2147fa1b61c1ca
verbal-noun <kaifahsan420@gmail.com>
2020-08-21 01:19:00 +1000

    Adding grid view toggle mode

---
+++ Assets/Scripts/GridView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GridView : MonoBehaviour
+{
+    public KeyCode toggle;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(toggle))
+        {
+            this.GetComponent<SpriteRenderer>().enabled = 
+                !this.GetComponent<SpriteRenderer>().enabled;
+        }
+    }
+
+    
+}
