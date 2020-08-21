91f04183d79006a83c47c2bb89f162a8d4a16387
verbal-noun <kaifahsan420@gmail.com>
2020-08-22 02:02:23 +1000

    Implemented pause menu template

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GamePause = false;
+    public KeyCode pauseButton;
+    private void Update() {
+        if(Input.GetKeyDown(pauseButton)) {
+            if (GamePause) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    void Resume() {
+        
+    }
+
+    void Pause() {
+
+    }
+}
