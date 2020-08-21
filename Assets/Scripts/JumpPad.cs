fee235c146d6f7df95a86c29f742c0715a18fd61
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-08-22 01:36:02 +1000

    added player prefab

---
+++ Assets/Scripts/JumpPad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpSpeed = 20f;
+
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player"){
+            CharacterController playerScript = other.gameObject.GetComponent<CharacterController>();
+            playerScript.Jump(jumpSpeed);
+        }
+    }
+}
