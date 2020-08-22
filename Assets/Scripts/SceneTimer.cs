dd250e140f088621e89d115c19b3d74ec4036461
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-08-22 23:47:49 +1000

    added transition scenes

---
+++ Assets/Scripts/SceneTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float time = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0 ){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        }
+    }
+}
