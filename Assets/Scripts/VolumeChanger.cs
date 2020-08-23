efa86a5b8537dbe95c279703edb2f7146458242f
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-08-23 15:46:53 +1000

    implemented volume slider

---
+++ Assets/Scripts/VolumeChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource audioSource;
+
+    private float musicVolume = 0.5f;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.volume = musicVolume;
+    }
+
+    public void setVolume(float vol){
+        musicVolume = vol;
+    }
+}
