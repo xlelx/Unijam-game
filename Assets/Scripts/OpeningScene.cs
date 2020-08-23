7c7e4974e23227dd6b3cad9fb47f514466b8a5e2
verbal-noun <kaifahsan420@gmail.com>
2020-08-23 19:08:03 +1000

    Complete player animation

---
+++ Assets/Scripts/OpeningScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningScene : MonoBehaviour
+{
+    public Rigidbody2D hansel;
+    public Rigidbody2D gretel;
+    public Animator hanselAnim;
+    public Animator gretelAnim;
+    bool playingAnimation = false;
+    public void playScene() {
+        playingAnimation = true; 
+    }
+
+    private void Update() {
+        if(playingAnimation) {
+            
+            // Adding velocity 
+            hansel.velocity = new Vector2(3, 0); 
+            gretel.velocity = new Vector2(-3, 0); 
+
+            // Set the animation 
+            hanselAnim.SetFloat("speed", Mathf.Abs(3 * 3));
+            gretelAnim.SetFloat("speed", Mathf.Abs(3 * 3));
+        }
+    }
+}
