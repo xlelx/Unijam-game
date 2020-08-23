d0e1e19345501b960761a8c1c350c5aecff62cb2
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-08-23 21:09:26 +1000

    added door animations

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    
+
     BoxCollider2D doorCollider;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         doorCollider = this.GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Door opening animation here 
+        if (other.tag == "Player")
+        {
+            // Door opening animation here 
+            animator.SetBool("openDoor", true);
+            
+            
+
+        }
 
     }
 
-    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player") {
+            animator.SetBool("openDoor", false);
+            other.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+
 }
