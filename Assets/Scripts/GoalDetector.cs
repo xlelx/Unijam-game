3493e9ec694c49dac54c41f125db82e8a80322fd
Khant Thurein Han <khantthurein@student.unimelb.edu.au>
2020-08-23 19:08:24 +1000

    implemented collectable and portal opening animation

--- Assets/Scripts/GoalDetector.cs
+++ Assets/Scripts/GoalDetector.cs
@@ -5,15 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class GoalDetector : MonoBehaviour
 {
-    private int numInGoal = 0;  
+    private int numInGoal = 0; 
+    public bool portalOpen = true;
+
+    private Animator animator;
+
+    private void Start(){
+        animator = GetComponent<Animator>();
+        animator.SetBool("isOpen", portalOpen);
+    }
     private void Update() {
-        if (numInGoal == 2){
+        
+        if (portalOpen && numInGoal == 2){
             //TODO animations or somestuff before loading the next level
             SoundManagerScript.PlaySound("portal");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             Debug.Log("Level Complete!");
         }    
-        
+
     }
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player"){
@@ -25,5 +34,11 @@ public class GoalDetector : MonoBehaviour
         if (other.tag == "Player"){
             numInGoal--;
         }
+    }
+
+    public void OpenPortal(){
+        portalOpen = true;
+        animator.SetBool("isOpen", true);
+        
     }
 }
