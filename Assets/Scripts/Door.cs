0a961c1d8cc0fd151934a20792b742510f0296cb
verbal-noun <kaifahsan420@gmail.com>
2020-08-23 20:28:17 +1000

    Finishing ending cutscene

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public KeyCode open;
     
     BoxCollider2D doorCollider;
     // Start is called before the first frame update
@@ -13,21 +12,11 @@ public class Door : MonoBehaviour
         doorCollider = this.GetComponent<BoxCollider2D>();
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            if (Input.GetKeyDown(open))
-            {
-                doorCollider.enabled = !doorCollider.enabled; 
-                Debug.Log("Reached");
-            }
-        }
+        // Door opening animation here 
+
     }
-    // Update is called once per frame
-    void Update()
-    {
-     
-    }
+
     
 }
